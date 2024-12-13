9b76a9562ef8231ce24035eb9c13947c85a0b377
Sebastina <sebane1@thavea.com>
2024-12-13 13:24:01 -0600

    Add properties for indicator distance and keeping objectives off the map.

--- QuestObjective.cs
+++ QuestObjective.cs
@@ -7,22 +7,26 @@ namespace RoleplayingQuestCore
         string _objective = "";
         Vector3 _coordinates = new Vector3();
         Vector3 _rotation = new Vector3();
-        QuestPointType typeOfQuestPoint = QuestPointType.NPC;
+        QuestPointType _typeOfQuestPoint = QuestPointType.NPC;
         ObjectiveStatusType _objectiveStatus = ObjectiveStatusType.Complete;
         ObjectiveTriggerType _typeOfObjectiveTrigger = ObjectiveTriggerType.NormalInteraction;
         Dictionary<string, Transform> _npcStartingPositions = new Dictionary<string, Transform>();
         List<QuestText> _questText = new List<QuestText>();
         List<QuestObjective> _subObjectives = new List<QuestObjective>();
-        private int taskRepetitions = 0;
         string triggerText = "";
         bool _objectiveCompleted = false;
         private bool _isAPrimaryObjective = true;
+
+        private bool _dontShowOnMap = false;
+        private float _minimum3dIndicatorDistance = 48;
+
+
 
         public int TerritoryId { get => territoryId; set => territoryId = value; }
         public string Objective { get => _objective; set => _objective = value; }
         public Vector3 Coordinates { get => _coordinates; set => _coordinates = value; }
         public List<QuestText> QuestText { get => _questText; set => _questText = value; }
-        public QuestPointType TypeOfQuestPoint { get => typeOfQuestPoint; set => typeOfQuestPoint = value; }
+        public QuestPointType TypeOfQuestPoint { get => _typeOfQuestPoint; set => _typeOfQuestPoint = value; }
         public ObjectiveStatusType ObjectiveStatus { get => _objectiveStatus; set => _objectiveStatus = value; }
         public ObjectiveTriggerType TypeOfObjectiveTrigger { get => _typeOfObjectiveTrigger; set => _typeOfObjectiveTrigger = value; }
         public string TriggerText { get => triggerText; set => triggerText = value; }
@@ -31,6 +35,8 @@ namespace RoleplayingQuestCore
         public List<QuestObjective> SubObjectives { get => _subObjectives; set => _subObjectives = value; }
         public bool ObjectiveCompleted { get => _objectiveCompleted; }
         public bool IsAPrimaryObjective { get => _isAPrimaryObjective; set => _isAPrimaryObjective = value; }
+        public float Minimum3dIndicatorDistance { get => _minimum3dIndicatorDistance; set => _minimum3dIndicatorDistance = value; }
+        public bool DontShowOnMap { get => _dontShowOnMap; set => _dontShowOnMap = value; }
 
         public List<string> EnumerateCharactersAtObjective()
         {
