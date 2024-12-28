daf58b5cbf59fda4ce89256a8bf5ce7db0ded6c7
Sebastina <sebane1@thavea.com>
2024-12-27 23:18:09 -0600

    Add d20 support.

--- BranchingChoice.cs
+++ BranchingChoice.cs
@@ -1,32 +1,30 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
 namespace RoleplayingQuestCore
 {
     public class BranchingChoice
     {
-        string choiceText = "";
-        BranchingChoiceType choiceType;
-        RoleplayingQuest roleplayingQuest = new RoleplayingQuest();
-        int eventToJumpTo = 0;
-
-        public int EventToJumpTo { get => eventToJumpTo; set => eventToJumpTo = value; }
-        public RoleplayingQuest RoleplayingQuest { get => roleplayingQuest; set => roleplayingQuest = value; }
-        public BranchingChoiceType ChoiceType { get => choiceType; set => choiceType = value; }
-        public string ChoiceText { get => choiceText; set => choiceText = value; }
+        string _choiceText = "";
+        BranchingChoiceType _choiceType;
+        RoleplayingQuest _roleplayingQuest = new RoleplayingQuest();
+        int _eventToJumpTo = 0;
+        int _eventToJumpToFailure = 0;
+        int _minimumDiceRoll = 0;
+        public int EventToJumpTo { get => _eventToJumpTo; set => _eventToJumpTo = value; }
+        public RoleplayingQuest RoleplayingQuest { get => _roleplayingQuest; set => _roleplayingQuest = value; }
+        public BranchingChoiceType ChoiceType { get => _choiceType; set => _choiceType = value; }
+        public string ChoiceText { get => _choiceText; set => _choiceText = value; }
 
         #region Legacy
         [Obsolete("This is no longer used. Please use EventToJumpTo property")]
-        public int DialogueToJumpTo { set => eventToJumpTo = value; }
+        public int DialogueToJumpTo { set => _eventToJumpTo = value; }
+        public int EventToJumpToFailure { get => _eventToJumpToFailure; set => _eventToJumpToFailure = value; }
+        public int MinimumDiceRoll { get => _minimumDiceRoll; set => _minimumDiceRoll = value; }
         #endregion
 
         public enum BranchingChoiceType
         {
             SkipToEventNumber,
-            BranchingQuestline
+            BranchingQuestline,
+            RollD20ThenSkipToEventNumber
         }
     }
 }
