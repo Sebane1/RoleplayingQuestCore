5ec3c8d62829ea58ad3a3d483b41cf8294c94208
Sebastina <sebane1@thavea.com>
2024-12-28 13:32:15 -0600

    Add more randomized choice options.

--- BranchingChoice.cs
+++ BranchingChoice.cs
@@ -8,23 +8,27 @@ namespace RoleplayingQuestCore
         int _eventToJumpTo = 0;
         int _eventToJumpToFailure = 0;
         int _minimumDiceRoll = 0;
+
+        List<int> _randomizedEventsToSkipTo = new List<int>() { 0 };
         public int EventToJumpTo { get => _eventToJumpTo; set => _eventToJumpTo = value; }
         public RoleplayingQuest RoleplayingQuest { get => _roleplayingQuest; set => _roleplayingQuest = value; }
         public BranchingChoiceType ChoiceType { get => _choiceType; set => _choiceType = value; }
         public string ChoiceText { get => _choiceText; set => _choiceText = value; }
+        public int EventToJumpToFailure { get => _eventToJumpToFailure; set => _eventToJumpToFailure = value; }
+        public int MinimumDiceRoll { get => _minimumDiceRoll; set => _minimumDiceRoll = value; }
+        public List<int> RandomizedEventToSkipTo { get => _randomizedEventsToSkipTo; set => _randomizedEventsToSkipTo = value; }
 
         #region Legacy
         [Obsolete("This is no longer used. Please use EventToJumpTo property")]
         public int DialogueToJumpTo { set => _eventToJumpTo = value; }
-        public int EventToJumpToFailure { get => _eventToJumpToFailure; set => _eventToJumpToFailure = value; }
-        public int MinimumDiceRoll { get => _minimumDiceRoll; set => _minimumDiceRoll = value; }
         #endregion
 
         public enum BranchingChoiceType
         {
             SkipToEventNumber,
             BranchingQuestline,
-            RollD20ThenSkipToEventNumber
+            RollD20ThenSkipToEventNumber,
+            SkipToEventNumberRandomized
         }
     }
 }
