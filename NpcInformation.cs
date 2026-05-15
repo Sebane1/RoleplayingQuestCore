df35e97a79466052240da29e3f52df48d7aced01
Sebastina <sebane1@thavea.com>
2026-05-15 12:45:11 -0500

    Add nameplate alias.

--- NpcInformation.cs
+++ NpcInformation.cs
@@ -4,8 +4,12 @@ namespace RoleplayingQuestCore
     {
         string _npcName = "NPC Name Here";
         string _appearanceData = "none.mcdf";
+        bool _hideNameplate = true;
+        string _nameplateAlias = "";
 
         public string NpcName { get => _npcName; set => _npcName = value; }
         public string AppearanceData { get => _appearanceData; set => _appearanceData = value; }
+        public bool HideNameplate { get => _hideNameplate; set => _hideNameplate = value; }
+        public string NameplateAlias { get => _nameplateAlias; set => _nameplateAlias = value; }
     }
 }
